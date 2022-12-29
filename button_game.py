c5c7e8b548ba169c469bc7fa4cf718f3197961db
Alex Korn <alex@iotiq.de>
2022-12-29 13:44:39 +0100

    add waiting print

--- button_game.py
+++ button_game.py
@@ -2,15 +2,17 @@ from simple import GameClient, sleep
 # from led import GameLED
 from random import randint
 
-topic = input("choose a lobby name to play against eachother: ")
+# topic = input("choose a lobby name to play against eachother: ")
+topic = "ok"
 my_client_id = str(randint(0, 10000))
 gclient = GameClient(my_client_id, game_lobby=topic, main=None)
 gclient.mqtt_client.loop_start()
 
 for i in range(10):
-    sleep(1)
     if gclient.main is not None:
         break
+    print("waiting for other client...")
+    sleep(1)
 if gclient.main is None:
     exit("timeout")
 
